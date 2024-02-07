06d8015e60102b51bdcdd0c14e743a9e5705dd6f
Thiago Berne <tberne@users.noreply.github.com>
2024-02-07 10:55:03 -0300

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -15,11 +15,34 @@ function Make-Diff() {
 
 	$FilePath = "C:\Users\tberne\OneDrive\Documentos\Issues\$Dir\$Issue.diff"
 	
+	if(-not(Test-Path "C:\Users\tberne\OneDrive\Documentos\Issues\$Dir")) {
+		New-Item -Type Directory -Path "C:\Users\tberne\OneDrive\Documentos\Issues\$Dir" | Out-Null
+	}
+	else {
+		if(-not (Test-Path "C:\Users\tberne\OneDrive\Documentos\Issues\$Dir" -PathType Container)) {
+			throw "ERRO! 'C:\Users\tberne\OneDrive\Documentos\Issues\$Dir' existe e nao e um diretorio!"
+		}
+	}
+
 	Push-Location -Path C:\Lumis\trunk\svnroot\Lumis_PortalJava
 	svn diff --cl $ChangeList > $FilePath
 	Pop-Location
+
+	Invoke-Item -Path "C:\Users\tberne\OneDrive\Documentos\Issues\$Dir"
 }
 
 function Mvn() {
-	C:\Apps\apache-maven-3.9.6\bin\mvn.cmd $args
+	C:\Apps\apache-maven-3.9.6\bin\mvn.cmd @args
+}
+
+function Python2() {
+	C:\Python27\python.exe @args
+}
+
+function Svn-Merge() {
+	Python2 C:\Apps\svnmerge\svnmerge.py @args
+}
+
+function ll() {
+	Get-ChildItem @args
 }
